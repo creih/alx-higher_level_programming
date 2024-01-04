790144009972e1014ae729aa1a5e38e4c4f11182
creih <iherr.9@gmail.com>
2024-01-04 21:15:51 +0200

    checked for neg ints

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    return (int(number % 10))
+    if number > 0:
+        number = int(number % 10)
+        return (number)
+    else:
+        number = int((number * -1) % 1)
+        return (number * -1)
