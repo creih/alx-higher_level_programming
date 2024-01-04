5e99de3c582f85e933afd5ce186937d452c62145
creih <iherr.9@gmail.com>
2024-01-04 21:28:21 +0200

    checked for neg ints and removed style cast

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    if number > 0:
-        number = int(number) % 10
-        return (number)
+    if number >= 0:
+        digi = number % 10
     else:
-        number = int(number * -1) % 10
-        return (number * -1)
+        digi = (-number % 10)
+    print(digi, end='')
+    return (digi)
