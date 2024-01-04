8fafa9c0d63d7374cae3af8221ea5ac55378f54e
creih <iherr.9@gmail.com>
2024-01-04 21:59:02 +0200

    task 12 ready for checker

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for its in range(1, 100):
+        if (its % 3 == 0 and its % 5 == 0):
+            print("FizzBuzz", end=' ')
+        elif (its % 3 == 0):
+            print("Fizz", end=' ')
+        elif (its % 5 == 0):
+            print("Buzz", end=' ');
+        else:
+            print(its, end=' ')
+        
