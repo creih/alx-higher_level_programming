e06f6731c4e33b8ed50d6e1f8ba191301bfe4c80
creih <iherr.9@gmail.com>
2024-01-04 09:40:39 +0200

    task 1 done but nt checking for the - ints

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,4 +2,10 @@
 import random
 number = random.randint(-10000, 10000)
 digits = int(number % 10)
-print(digits)
+print(number)
+if digits > 5:
+    print(f"Last digit of {number} is {digits} and is greater than 5")
+elif digits == 0:
+    print(f"Last digit of {number} is {digits} and is 0")
+else:
+    print(f"Last digit of {number} is {digits} and is less than 6 and not 0")
