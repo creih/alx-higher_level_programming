13612aa2900053bf1c7b829023f10a3add19faaf
creih <iherr.9@gmail.com>
2024-01-04 21:20:29 +0200

    checked for neg ints and used style cast

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def print_last_digit(number):
     if number > 0:
-        number = int(number % 10)
+        number = int(number) % 10
         return (number)
     else:
-        number = int((number * -1) % 1)
+        number = int(number * -1) % 10
         return (number * -1)
