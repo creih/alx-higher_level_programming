009b5021b8664d23bc629bcb030513d979413666
creih <iherr.9@gmail.com>
2024-01-04 21:50:55 +0200

    task 11 ready for checker and all conditions acc for

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def pow(a, b):
-    for tymz in range(b - 1):
-        a *= a
-    return (a)
+    if b > 0:
+        return (a ** b)
+    elif b == 0:
+        return (1)
+    else:
+        return (a ** b)
