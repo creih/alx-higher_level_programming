3d1e2eb5993b165e3709e530bc943f37588bac93
creih <iherr.9@gmail.com>
2024-01-05 20:42:44 +0200

    task 0 added if __name__

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 from add_0 import add
-a = 1
-b = 2
-res = add(a, b)
-print("{0} + {1} = {2}".format(a, b, res))
+def main():
+    """
+    this is the main
+    """
+    a = 1
+    b = 2
+    res = add(a, b)
+    print("{:d} + {:d} = {:d}".format(a, b, res))
+if __main__ == "__main__":
+    main()
