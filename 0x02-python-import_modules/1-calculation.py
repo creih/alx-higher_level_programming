dba96d0fc6f26d9a14ef0fbc5b0baa0a29968ed8
creih <iherr.9@gmail.com>
2024-01-05 21:26:24 +0200

    task 1 redone

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
 from calculator_1 as c
-
-a = 10
-b = 5
-
-add_result = c.add(a, b)
-sub_result = c.sub(a, b)
-mul_result = c.mul(a, b)
-div_result = c.div(a, b)
-
-print("{:d} + {:d} = {:d}".format(a, b, add_result))
-print("{:d} - {:d} = {:d}".format(a, b, sub_result))
-print("{:d} * {:d} = {:d}".format(a, b, mul_result))
-print("{:d} / {:d} = {:d}".format(a, b, div_result))
+if __name__ = "__main__"
+    a = 10
+    b = 5
+    add_result = c.add(a, b)
+    sub_result = c.sub(a, b)
+    mul_result = c.mul(a, b)
+    div_result = c.div(a, b)
+    print("{:d} + {:d} = {:d}".format(a, b, add_result))
+    print("{:d} - {:d} = {:d}".format(a, b, sub_result))
+    print("{:d} * {:d} = {:d}".format(a, b, mul_result))
+    print("{:d} / {:d} = {:d}".format(a, b, div_result))
