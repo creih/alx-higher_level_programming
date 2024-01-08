1a085848369415d2851747b0fe506067d7eb6711
creih <iherr.9@gmail.com>
2024-01-08 21:39:27 +0200

    task 1 response

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,8 @@
+def element_at(my_list, idx):
+    if idx < 0 or idx >= len(my_list):
+        return None
+    return my_list[idx]
+
+
+if __name__ = "__main__":
+    print("Element at index {0} is {1}".format(idx, element_at(my_list, idx)))
