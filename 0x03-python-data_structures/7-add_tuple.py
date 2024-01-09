7ed06e4c6a64e2122b3a5da4372935848e627a34
creih <iherr.9@gmail.com>
2024-01-09 21:02:54 +0200

    added task typo in tupple rather than tuple

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-def add_tupple(tupple_a=(), tupple_b=()):
-    res = tuple(map(lambda i, j: i + j, tuple_a, tuple_b))
+def add_tupple(tuple_a=(), tuple_b=()):
+    res = tuple_a + tuple_b
+    return res
