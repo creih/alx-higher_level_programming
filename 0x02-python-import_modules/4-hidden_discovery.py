08a4eb6d2afb3360e6129766d890a5931c45ee69
creih <iherr.9@gmail.com>
2024-01-09 21:58:50 +0200

    added task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,13 +2,16 @@
 import dis
 import hidden_4
 
+
 def discover_names(module):
     """
     this is a function
+
     """
     names = [name for name in dir(module) if not name.startswith("__")]
     names.sort()
     return names
+
 
 if __name__ == "__main__":
     module_names = discover_names(hidden_4)
