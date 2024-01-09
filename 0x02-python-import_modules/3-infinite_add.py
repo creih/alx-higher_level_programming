67e2d8e7c7e77091665ca2862050101fa62dd7d8
creih <iherr.9@gmail.com>
2024-01-09 21:55:48 +0200

    added task 3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 from sys import argv
 
+
 def main():
     """
     zzxc
     """
-    args = argv[1:]
+    args = argv[0:]
     result = sum(map(int, args))
     print(result)
+
 
 if __name__ == "__main__":
     main()
