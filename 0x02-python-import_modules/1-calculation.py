f587b9e7957a71f98a805a15a039f4b6a8169bc5
creih <iherr.9@gmail.com>
2024-01-09 21:50:40 +0200

    added task 1

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-import calculator_1 as c
+from calculator_1 import add, sub, mul, div
 
 
 if __name__ == "__main__":
     a = 10
     b = 5
-    add_result = c.add(a, b)
-    sub_result = c.sub(a, b)
-    mul_result = c.mul(a, b)
-    div_result = c.div(a, b)
+    add_result = add(a, b)
+    sub_result = sub(a, b)
+    mul_result = mul(a, b)
+    div_result = div(a, b)
     print("{:d} + {:d} = {:d}".format(a, b, add_result))
     print("{:d} - {:d} = {:d}".format(a, b, sub_result))
     print("{:d} * {:d} = {:d}".format(a, b, mul_result))
