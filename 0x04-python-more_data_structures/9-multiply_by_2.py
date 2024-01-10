c2df76f01e0a36c19edb76124f4ddaec08003514
creih <iherr.9@gmail.com>
2024-01-10 22:02:59 +0200

    task 9 ready for check added return

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    new_dic = [len(a_dictionary)]
-    for umubare in range(len(a_dictionary)):
-        new_dic[umubare] = a_dictionary[umubare] * 2
+    new_dic = {key: value * 2 for key, value in a_dictionary.items()}
     return new_dic
