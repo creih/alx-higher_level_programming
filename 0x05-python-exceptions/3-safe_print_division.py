93f9fffda21355d5250f4245e15a213aef7215af
creih <iherr.9@gmail.com>
2024-01-22 20:34:32 +0200

    task 4 and 3

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -2,7 +2,7 @@
 def safe_print_division(a, b):
     try:
         subizo = a / b
-    except ZeroDivisionError:
+    except (ZeroDivisionError, TypeError):
         return None
     else:
         return subizo
