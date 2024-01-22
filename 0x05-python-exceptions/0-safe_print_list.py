d78adb9e9af2dbdab2494b921f27355f7b9d74d3
creih <iherr.9@gmail.com>
2024-01-22 12:40:50 +0200

    removed the indexerror specification

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
         for item in range(x):
             print(my_list[item], sep='')
             bara += 1
-    except IndexError:
+    except:
         print()
     finally:
         print("")
