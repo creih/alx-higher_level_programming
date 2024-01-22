d11fe30b28050524d9dd5373d2b97b814668b322
creih <iherr.9@gmail.com>
2024-01-22 13:58:51 +0200

    used end instead of sep

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,7 +3,7 @@ def safe_print_list(my_list=[], x=0):
     bara = 0
     try:
         for item in range(x):
-            print(my_list[item], sep='')
+            print(my_list[item], end='')
             bara += 1
     except (IndexError, ...):
         print()
