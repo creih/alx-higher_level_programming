34fe0e06567c0295867a0324362147971c9cfc6e
creih <iherr.9@gmail.com>
2024-01-22 21:13:14 +0200

    task 101

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_function(fct, *args):
+    try:
+        result = fct(*args)
+        return result
+    except Exception as e:
+        import sys
+        print(f"Exception: {e}", file=sys.stderr)
+        return None
