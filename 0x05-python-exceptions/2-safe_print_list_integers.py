bc27dbbbfd1ddb888f048e45849225c578c77a6b
creih <iherr.9@gmail.com>
2024-01-22 20:30:43 +0200

    task 4

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -7,8 +7,8 @@ def safe_print_list_integers(my_list=[], x=0):
             if type(value) is int:
                 print("{:d}".format(value), end="")
                 bara += 1
-    except IndexError:
-        print("list index out of range")
+    except IndexError as ie:
+        print(f"Error: {ie}")
     except (ValueError, TypeError):
         pass
     finally:
---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    result_list = []
+
+    for i in range(list_length):
+        try:
+            element_1 = my_list_1[i]
+            element_2 = my_list_2[i]
+
+            if type(element_1) not in (int, float) or type(element_2) not in (int, float):
+                raise TypeError("wrong type")
+
+            result = element_1 / element_2
+
+        except TypeError as t_e:
+            print(t_e)
+            result = 0
+        except ZeroDivisionError:
+            print("division by 0")
+            result = 0
+        except IndexError:
+            print("out of range")
+            result = 0
+        finally:
+            result_list.append(result)
+
+    return result_list
