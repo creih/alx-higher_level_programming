579f2aa3f0b0942165aa621207309e4809165f5c
creih <iherr.9@gmail.com>
2024-01-22 14:06:47 +0200

    removed ...

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
         for item in range(x):
             print(my_list[item], end='')
             bara += 1
-    except (IndexError, ...):
+    except IndexError:
         pass
     finally:
         print()
