13420f7a5d265c71b19fc372d189857dea483598
creih <iherr.9@gmail.com>
2024-01-22 15:08:45 +0200

    task 2

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,14 @@
+#!/src/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    bara = 0
+    try:
+        for ins in range(x):
+            value = my_list[ins]
+            if type(value) is int:
+                print("{:d}".format(value), end="")
+                bara += 1
+    except (IndexError, ValueError, TypeError):
+        pass
+    finally:
+        print()
+    return bara
