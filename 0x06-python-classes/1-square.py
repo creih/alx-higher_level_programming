0d564af12def4e950f24f33b27c5b59d75637ae4
creih <iherr.9@gmail.com>
2024-01-23 17:07:10 +0200

    task 2

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+class Square:
+    """Square class.
+    this is diff from the one above by size.
+    Attributes:
+        size (int): Private square int.
+
+    """
+    def __init__(self, size):
+        """Initialisation.
+        Args:
+        size: size of the square
+
+        """
+        self.size = size
