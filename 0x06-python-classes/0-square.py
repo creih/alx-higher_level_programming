6ac673bca18eadfaf7132b32409fd16d53828740
creih <iherr.9@gmail.com>
2024-01-23 16:37:37 +0200

    task 1 redo

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -2,5 +2,10 @@
 class Square:
     """this is empty class of square"""
     def __init__(self):
-        """this is the initialisation method"""
+        """this is the initialisation method.
+        Args:
+            self: for this
+        Return: NOne
+        Raises: None
+        """
         pass
