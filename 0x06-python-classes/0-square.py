e36fc1e3882b3e26cf39d7f57ae03b709a9ef91e
creih <iherr.9@gmail.com>
2024-01-23 17:12:31 +0200

    rememnered the exec thingy

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 class Square:
     """this is empty class of square.
-    This is my empty square calss implem.
+    This is my empty square calss implementation.
+
     """
     pass
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -5,7 +5,6 @@ class Square:
     this is diff from the one above by size.
     Attributes:
         size (int): Private square int.
-
     """
     def __init__(self, size):
         """Initialisation.
