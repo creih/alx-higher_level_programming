cf940b63bca43f6d61a417f413c432a361e5adad
creih <iherr.9@gmail.com>
2024-01-23 21:35:19 +0200

    redid the doc of task 0 redone

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -2,9 +2,10 @@
  """this is empty class of square.
     This is my empty square calss implementation.
     """
+
+
 class Square:
     """this is empty class of square.
     This is my empty square calss implementation.
-
     """
     pass
