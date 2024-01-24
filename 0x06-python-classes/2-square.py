da72fc466ec047ba42cfdc3634acca5c83d482de
creih <iherr.9@gmail.com>
2024-01-24 08:10:31 +0200

    redid the doc of task 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
+    """ this class with a private size field"""
 class Square:
 """Class to define a square.
-
-    This class represents a square and has a private instance attribute `size`.
-    The size is validated during instantiation.
-
-    Attributes:
-        size (int): Private instance attribute representing the size of the square.
+class represents square and has private instance attribute `size`.
+The size is validated during instantiation.
+Attributes:
+    size (int): Private instance attribute representing size of square.
 
 """
-
     def __init__(self, size=0):
         """Initializes a new Square instance with an optional size.
 
