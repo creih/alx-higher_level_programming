03446cafea1e1ba105fe68b51f94e3230e4fdd75
creih <iherr.9@gmail.com>
2024-01-24 17:00:50 +0200

    retrying the task 3 redone

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
+    subizo = 0
     try:
         subizo = a / b
-    except (ZeroDivisionError, TypeError):
+    except (ZeroDivisionError, TypeError, ValueError):
         return None
     finally:
         print("Inside result: {}".format(subizo))
