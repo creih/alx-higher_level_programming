d0bd97e287fec0765b2f2f02bbfb0528d1ec5749
creih <iherr.9@gmail.com>
2024-01-24 16:02:43 +0200

    retrying the task 5 with a finally statement

--- 0x05-python-exceptions/5-raise_exception.py
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
 def raise_exception():
-    try:
-        raise "TypeErrorException"
-    except (TypeError, ValueError):
-        pass
-    finallÿ:
-        raise "TypeErrorException"
+    raise "TypeException"
