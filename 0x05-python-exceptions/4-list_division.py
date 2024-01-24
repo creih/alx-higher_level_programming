a6989019137139115eaddc1abda5a04561a7367a
creih <iherr.9@gmail.com>
2024-01-24 16:14:29 +0200

    retrying the task 4 indentation issue fixed

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -8,7 +8,7 @@ def list_division(my_list_1, my_list_2, list_length):
             e2 = my_list_2[i]
 
             if type(e1) not in (int, float) or type(e2) not in (int, float):
-                    raise TypeError("wrong type")
+                raise TypeError("wrong type")
 
             result = e1 / e2
 
