b66d16a8665e7d1cf085716cc17ff1553791dfb7
creih <iherr.9@gmail.com>
2024-01-25 09:56:51 +0200

    commited task 8 using --chmod

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str = str.split(',')[2][1:28] + str[-13:-8] + str.split(',')[-1][-12:-8]
+str = str.split(',')[2][1:28] + str.split(',')[-1][-23:-17] + str[0:6]
 print(str)
