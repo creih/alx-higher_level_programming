7891d89e73e82bcdc2322f3a7842a51fdd5883fb
creih <iherr.9@gmail.com>
2024-02-03 01:01:11 +0200

    task 1

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,33 +1,30 @@
 #!/usr/bin/python3
+import unittest
+
 def add_integer(a, b=98):
     """this is a python function to add 2 ints"""
     if not (isinstance(a, (int, float))):
         raise TypeError("a must be an integer")
     if not (isinstance(b, (int, float))):
         raise TypeError("b must be an integer")
+        a = int(a)
+        b = int(b)
     return (int(a) + int(b))
 
-# # Test cases
-# try:
-#     result1 = add_integer(5, 3)
-#     print(f"Test Case 1: {result1}")  # Output: 8
-# except TypeError as e:
-#     print(f"Test Case 1: {e}")
 
-# try:
-#     result2 = add_integer(2.5, 3)
-#     print(f"Test Case 2: {result2}")  # Output: 5
-# except TypeError as e:
-#     print(f"Test Case 2: {e}")
+# class TestAddition(unittest.TestCase):
+#     """this is the tests class"""
+#     def test_add_positive_numbers(self):
+#         result = add_numbers(3, 5)
+#         self.assertEqual(result, 8)
 
-# try:
-#     result3 = add_integer("10", 5)
-#     print(f"Test Case 3: {result3}")  # TypeError will be raised
-# except TypeError as e:
-#     print(f"Test Case 3: {e}")
+#     def test_add_negative_numbers(self):
+#         result = add_numbers(-2, -4)
+#         self.assertEqual(result, -6)
 
-# try:
-#     result4 = add_integer(7, "abc")
-#     print(f"Test Case 4: {result4}")  # TypeError will be raised
-# except TypeError as e:
-#     print(f"Test Case 4: {e}")
+#     def test_add_mixed_numbers(self):
+#         result = add_numbers(10, -3)
+#         self.assertEqual(result, 7)
+
+# if __name__ == '__main__':
+#     unittest.main()
