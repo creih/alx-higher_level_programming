30594be91c68fb5a261596058633faf22ff011c8
creih <iherr.9@gmail.com>
2024-02-03 01:03:13 +0200

    task 1 pycode ready

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import unittest
 
+
 def add_integer(a, b=98):
     """this is a python function to add 2 ints"""
     if not (isinstance(a, (int, float))):
@@ -10,21 +11,3 @@ def add_integer(a, b=98):
         a = int(a)
         b = int(b)
     return (int(a) + int(b))
-
-
-# class TestAddition(unittest.TestCase):
-#     """this is the tests class"""
-#     def test_add_positive_numbers(self):
-#         result = add_numbers(3, 5)
-#         self.assertEqual(result, 8)
-
-#     def test_add_negative_numbers(self):
-#         result = add_numbers(-2, -4)
-#         self.assertEqual(result, -6)
-
-#     def test_add_mixed_numbers(self):
-#         result = add_numbers(10, -3)
-#         self.assertEqual(result, 7)
-
-# if __name__ == '__main__':
-#     unittest.main()
