87b4f710e92907c13968c564e598cf4cd33a43e0
creih <iherr.9@gmail.com>
2024-02-06 20:50:49 +0200

    task 6 done

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""this is my file for task 6."""
+
+
+def load_from_json_file(filename):
+    with open(filename, 'r', encoding='Utf-8') as doc:
+        for chars in doc:
+            json.dumps(chars)
+        doc.close()
