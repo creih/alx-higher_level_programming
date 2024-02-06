085235a525c6bda2795358bbe5830355b54f1f48
creih <iherr.9@gmail.com>
2024-02-06 23:17:41 +0200

    thids is 13

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""this will be the task 12 file"""
+
+
+def pascal_triangle(n):
+    """Generates Pascal's triangle up to the nth row."""
+    if n <= 0:
+        return []
+    triangle = []
+
+    for i in range(n):
+        row = []
+        
+        for j in range(i + 1):
+            if j == 0 or j == i:
+                element = 1
+            else:
+                element = triangle[i - 1][j - 1] + triangle[i - 1][j]
+            row.append(element)
+        
+        triangle.append(row)
+    
+    return triangle
+
