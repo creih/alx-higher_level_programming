09a92ce8d78962fac5908fec42444638eb5c3788
creih <iherr.9@gmail.com>
2024-02-06 06:57:10 +0200

    task 4 done

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This module will be for the task 0 of the 0x0B file I/O alx project"""
+import json
+
+
+def to_json_string(my_obj):
+    """this is the function to turn obj into json data"""
+    return (json.loads(my_obj))
