135df7cbdb1313a9e7046760b3318c9f7e233451
creih <iherr.9@gmail.com>
2024-02-06 22:10:59 +0200

    task 8 and 7 reviewed

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
 """this is for the task 7 in 0x0B"""
+
+
 from save_to_json_file import save_to_json_file
 from load_from_json_file import load_from_json_file
 import sys
 
-
-arguments = sys.argv[1:]
-try:
-    items = load_from_json_file('add_item.json')
-except FileNotFoundError:
-    items = []
-items.extend(arguments)
-save_to_json_file(items, 'add_item.json')
+while (True):
+    arguments = sys.argv[1:]
+    try:
+        items = load_from_json_file('add_item.json')
+    except FileNotFoundError:
+        items = []
+    items.extend(arguments)
+    save_to_json_file(items, 'add_item.json')
