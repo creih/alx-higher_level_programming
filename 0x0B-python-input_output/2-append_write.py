c27ef418d122a27a1c37714ac61bb8b4dcc5d020
creih <iherr.9@gmail.com>
2024-02-06 06:03:19 +0200

    this is the task 3

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module will be for the task 0 of the 0x0B file I/O alx project"""
+
+
+def append_write(filename="", text=""):
+    """this is the function to write text to file"""
+    with open(filename, 'a', encoding='Utf-8') as doc:
+        doc.write(text)
+    doc.close()
+    return len(text)
