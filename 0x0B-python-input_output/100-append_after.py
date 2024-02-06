a9b28b3ff6f1af57cbf7134b1f5a1c25f8e6f25f
creih <iherr.9@gmail.com>
2024-02-06 22:23:17 +0200

    advanced task 100

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,18 +1,13 @@
 #!/usr/bin/python3
 """this is for  the advanced task thing"""
-import json
 
 
 def append_after(filename="", search_string="", new_string=""):
     """Inserts a line of text to a file after each line containing a specific string."""
-    with open(filename, 'r+') as file:
-
-        lines = file.readlines()
-
+    with open(filename, 'r+',encoding='Utf-8') as doc:
+        lines = doc.readlines()
         for i, line in enumerate(lines):
-
             if search_string in line:
-
                 lines.insert(i + 1, new_string + '\n')
-        file.seek(0)
-        file.writelines(lines)
+        doc.seek(0)
+        doc.writelines(lines)
