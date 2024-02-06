6f1041b76d3f3a9d917b4eefe4bad522178138dc
creih <iherr.9@gmail.com>
2024-02-06 20:20:52 +0200

    task 5 done

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module will be for the task 0 of the 0x0B file I/O alx project"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """this is the function to turn obj into json data"""
+    with open(filename, 'w', 'Utf-8') as file:
+        json.dump(my_obj, filename)
+    file.closed()
