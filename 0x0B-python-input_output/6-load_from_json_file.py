701441b39971563423e1302dec2f5eb67555df4e
creih <iherr.9@gmail.com>
2024-02-06 21:15:37 +0200

    task 6 complete

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -6,4 +6,4 @@ import json
 def load_from_json_file(filename):
     """the function here gets an object for the file"""
     with open(filename, 'r', encoding='Utf-8') as docs:
-        objects = json.load(docs)
+        objects = json.loads(docs)
