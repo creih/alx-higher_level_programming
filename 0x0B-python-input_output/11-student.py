1469d318a8e0d4eb4db3eb1583a5becbe69ba105
creih <iherr.9@gmail.com>
2024-02-06 23:28:42 +0200

    thids is 11

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -5,16 +5,18 @@
 class Student:
     """Defines a student."""
     def __init__(self, first_name, last_name, age):
-        """Initializes a student with first name, last name, and age."""
+        """Initializes stud irst name, last name, age."""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
     def to_json(self, attrs=None):
-        """Retrieves a dictionary representation of a Student instance."""
+        """Retrieves dict rep of a Student instance."""
         if attrs is None:
             return self.__dict__
         else:
-            return {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)} 
+            for attr in attrs:
+                if hasattr(self, attr):
+                    return {attr: getattr(self, attr)  } 
     def reload_from_json(self, json):
         """Replaces all attributes of the Student instance."""
         for key, value in json.items():
