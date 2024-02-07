bb215a95091efa5a0c0ad2094dd53b5b9ca33cc3
creih <iherr.9@gmail.com>
2024-02-08 00:22:52 +0200

    this is task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+this is my response for task 6
+"""
+
+
+class BaseGeometry:
+    """this class is for area(.)"""
+    
+    def area():
+        """"this is the def"""
+        raise Exception("area() is not implemented")
