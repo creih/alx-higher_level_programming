c7088d6903424137863b0fafb1fdcc0259635333
creih <iherr.9@gmail.com>
2024-02-07 22:28:01 +0200

    task 0 redone

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -9,5 +9,5 @@ def lookup(obj):
     # Get the list of all attributes and methods
     attributes_and_methods = dir(obj)
     # Filter out attributes and methods starting with double underscores
-    filtered_attributes_and_methods = [item for item in attributes_and_methods if not item.startswith("__")]
+    filtered_attributes_and_methods = [item for item in attributes_and_methods if item.startswith("__")]
     return filtered_attributes_and_methods
