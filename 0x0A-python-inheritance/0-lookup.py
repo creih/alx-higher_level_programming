c393270d5227231b047c807d8fbc7eb834583a98
creih <iherr.9@gmail.com>
2024-02-07 22:15:41 +0200

    this is task 0

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -6,4 +6,8 @@ this is the task 0 file
 
 def lookup(obj):
     """this function returns a list of object."""
+    filt_attr = dir(obj)
+    for one in filt_attr:
+        if not one.startswith("__"):
+            filtered = one
     return obj
