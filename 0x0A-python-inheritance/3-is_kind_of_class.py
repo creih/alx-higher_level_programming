1f18f7d904f2903928dd5849d672008fc321085e
creih <iherr.9@gmail.com>
2024-02-07 23:55:28 +0200

    this is tas 3

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+file for task 3
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """this is the function for checking if is instance"""
+    return isinstance(obj, a_class)
