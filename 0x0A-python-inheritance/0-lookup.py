ab5c2aff36fa59ded22f7ab183bb10453b1fc862
creih <iherr.9@gmail.com>
2024-02-07 22:21:27 +0200

    this is task 0 IS NOT done

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -5,9 +5,9 @@ this is the task 0 file
 
 
 def lookup(obj):
-    """Return a list of available attributes and methods of an object."""
-    
-    attributes_and_methods = dir(obj)
-    
-    filtered_attributes_and_methods = [item for item in attributes_and_methods if not item.startswith("__")]
-    return filtered_attributes_and_methods
+    """this function returns a list of object."""
+    filt_attr = dir(obj)
+    for one in filt_attr:
+        if not one.startswith("__"):
+            filtered = one
+    return filtered
