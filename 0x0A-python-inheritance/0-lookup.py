eed09055f8faa9cf02c1996794e0f5c082ddf0e8
creih <iherr.9@gmail.com>
2024-02-07 22:35:53 +0200

    task 0 redone

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -7,5 +7,5 @@ this is the task 0 file
 def lookup(obj):
     """Return a list of available attributes and methods of an object."""
     atts = dir(obj)
-    filt = [item for item in attributes_and_methods if item.startswith("__")]
+    filt = [item for item in atts if item.startswith("__")]
     return filt
