8b7767df2e0f7c4254f5bbecba6f3b60df1a5912
creih <iherr.9@gmail.com>
2024-02-09 18:28:31 +0200

    task 2

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -8,7 +8,7 @@ def matrix_divided(matrix, div):
     """ this is my function to divide a matrix"""
     msg = "matrix must be a matrix (list of lists) of integers/floats"
     for umurongo in range(len(matrix)):
-        for individ in range(len(umurongo)):
+        for individ in umurongo:
             if not isinstance(matrix[umurongo][individ], (int, float)):
                 return (msg)
             matrix[umurongo][individ] /= div
