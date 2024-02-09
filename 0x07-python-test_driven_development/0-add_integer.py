43f8fa5887e72719b7d067bbbd96759bdd0ede21
creih <iherr.9@gmail.com>
2024-02-09 17:32:19 +0200

    i may have resolve the typerror msg tailoring thing for task 0

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -5,9 +5,11 @@ import unittest
 def add_integer(a, b=98):
     """this is a python function to add 2 ints"""
     if not (isinstance(a, (int, float))):
-        raise TypeError("a must be an integer")
+        print("a must be an integer")
+        return
     if not (isinstance(b, (int, float))):
-        raise TypeError("b must be an integer")
-        a = int(a)
-        b = int(b)
-    return (int(a) + int(b))
+        print("b must be an integer")
+        return
+    a = int(a)
+    b = int(b)
+    return (a + b)
