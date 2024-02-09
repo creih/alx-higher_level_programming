6b6dbd150ffda557a9c553a7925afe988330c207
creih <iherr.9@gmail.com>
2024-02-09 17:44:12 +0200

    i may have resolve the typerror msg tailoring thing for task 0

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -8,9 +8,9 @@ required.
 def add_integer(a, b=98):
     """this is a python function to add 2 ints"""
     if not (isinstance(a, (int, float))):
-        return ("a must be an integer")
+        return "a must be an integer"
     if not (isinstance(b, (int, float))):
-        return ("b must be an integer")
+        return "b must be an integer"
     a = int(a)
     b = int(b)
     return (a + b)
