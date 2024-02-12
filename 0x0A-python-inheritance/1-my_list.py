2aa7e75bbf52c83ed6a651b6ee1d770c8d64c2cf
creih <iherr.9@gmail.com>
2024-02-13 01:05:10 +0200

    the task1 redo

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -4,29 +4,10 @@ this is the file for task 1.
 """
 
 
-# class MyList(list):
-#     """
-#     thios is the required class for inheritting list
-#     """
-
-#     def print_sorted(self):
-#         """THIS PRINTS the list"""
-#         print(sorted(self))
 class MyList(list):
     """
     thios is the required class for inheritting list
     """
-    def __init__(self, *args):
-        """THIS PRINTS the list"""
-        super().__init__(*args)
-
-    def __str__(self):
-        """THIS PRINTS the list"""
-        return super().__str__()
-
-    def append(self, item):
-        """THIS PRINTS the list"""
-        super().append(item)
 
     def print_sorted(self):
         """THIS PRINTS the list"""
