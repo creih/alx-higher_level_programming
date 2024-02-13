cb0397b0c5e9de43804a4956057b1bcea3b8e254
creih <iherr.9@gmail.com>
2024-02-13 21:15:46 +0200

    this is the task 8

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -2,9 +2,10 @@
 """
 this is the file for task 8 class
 """
+base_geom = __import__(7-base_geometry).BaseGeometry 
 
 
-class Rectangle:
+class Rectangle(BaseGeometry):
     """this is the class for the rectangle class"""
     def __init__(self, width, height):
         """initialisation of Rectangle clas"""
