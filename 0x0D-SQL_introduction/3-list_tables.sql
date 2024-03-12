8a4a7edcdbb53f014c0c53bb27ff852c17cbd714
creih <iherr.9@gmail.com>
2024-03-12 15:39:23 -0700

    task 3 dialed down a nudge

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,8 +1,4 @@
 -- it is getting abit tougher now
--- we are displaying the tables of arg 1 of script
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <database_name>"
-    exit 1
-fi
-database="$1"
-mysql -e "USE $database; SHOW TABLES;"
+-- we are displaying the tables of arg 1 of s
+use '$1'
+show tables
