7a6e24041dc613fc2ed09d4880b03e4239516bf8
creih <iherr.9@gmail.com>
2024-03-12 15:26:04 -0700

    task 1 and 2

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,3 +1,3 @@
 -- yet another task about creating db
 -- still using the scripted querries
-create database hbtn_0c_0
+create database if not exists hbtn_0c_0
---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,3 @@
+-- this is task 3 now we del the db
+-- this time we get to use drop
+DROP DATABASE IF EXISTS hbtn_0c_0
