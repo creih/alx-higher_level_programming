20db119e52868a6693a5754618252bbea5313371
creih <iherr.9@gmail.com>
2024-03-14 07:03:48 -0700

    record 3 id was made 2 instead of 3

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -3,5 +3,5 @@
 CREATE TABLE IF NOT EXISTS second_table(id INT, name VARCHAR, score INT);
 INSERT INTO second_table VALUES(1, 'John', 10);
 INSERT INTO second_table VALUES(2, 'Alex', 3);
-INSERT INTO second_table VALUES(2, 'Bob', 14);
-INSERT INTO second_table VALUES(3, 'George', 8);
+INSERT INTO second_table VALUES(3, 'Bob', 14);
+INSERT INTO second_table VALUES(4, 'George', 8);
