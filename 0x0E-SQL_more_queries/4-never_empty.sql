977d57490dea8e3732cd2600076bada36372326b
creih <iherr.9@gmail.com>
2024-03-14 12:18:04 -0700

    task 4 removed = in default=1

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,3 +1,3 @@
 -- creatin of a table with notnull id
 -- will add NOT NULL  kuri specs za field ya id
-CREATE TABLE IF NOT NULL id_not_null(id INT NOT NULL DEFAULT 1, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS NULL id_not_null(id INT NOT NULL DEFAULT 1, name VARCHAR(256));
