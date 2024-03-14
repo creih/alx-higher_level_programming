83727b7b528dcfa8ac1d4b44c105a9e4b3ee2a0d
creih <iherr.9@gmail.com>
2024-03-14 11:39:41 -0700

    I was forgetting to add privileges

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,5 @@
 -- task 1 about creation of user
--- nakoresheje create user
-CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+-- nakoresheje create user, grant privileges na FLUSH o guhanagura memory
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
