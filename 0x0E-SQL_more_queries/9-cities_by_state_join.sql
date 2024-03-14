05421af768ea06effd44354a6e8f1c6cd4a82f83
creih <iherr.9@gmail.com>
2024-03-14 13:36:22 -0700

    task 9 about using one select for multiple tables

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,3 @@
+-- selecting from multiple tables with one select
+-- task 9
+SELECT cities.id, cities.name, states.name FROM cities, states WHERE cities.state_id = states.id ORDER BY cities.id ASC;
