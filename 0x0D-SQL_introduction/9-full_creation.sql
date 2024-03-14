38f9b0d511ffaf39c8909faaa03427f3f70da6d3
creih <iherr.9@gmail.com>
2024-03-14 07:00:11 -0700

    task 9 about creation and population of second_table

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,7 @@
+-- task 9 about creating table
+-- second table(id INT, name VARCHAR(256), score INT)
+CREATE TABLE IF NOT EXISTS second_table(id INT, name VARCHAR, score INT);
+INSERT INTO second_table VALUES(1, 'John', 10);
+INSERT INTO second_table VALUES(2, 'Alex', 3);
+INSERT INTO second_table VALUES(2, 'Bob', 14);
+INSERT INTO second_table VALUES(3, 'George', 8);
