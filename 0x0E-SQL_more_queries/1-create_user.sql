f449dc51a593fc588eda2ec9e75727852e227b22
creih <iherr.9@gmail.com>
2024-03-14 11:33:41 -0700

    task1 about user creation

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- task 1 about creation of user
 -- nakoresheje create user
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
