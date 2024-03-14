c2caea404ae12ba245cefef8085db6daa70187a1
creih <iherr.9@gmail.com>
2024-03-14 10:02:39 -0700

    task 10 selcting name and score only

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,3 +1,3 @@
 -- task 10 list all second_table' s record
 -- ndakoresha select * na order by
-SELECT * FROM second_table ORDER BY score ASC
+SELECT score, name FROM second_table ORDER BY score DESC
