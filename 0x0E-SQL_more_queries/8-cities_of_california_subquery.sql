4cf0f5234b5b03e0d3733bad4b70cb4aae8e92b3
creih <iherr.9@gmail.com>
2024-03-14 14:20:56 -0700

    task 8 removed spaces around =

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,3 @@
--- task 8
+-- task 8 about selecting multiple fields from diff tables
 -- using select cyane
-SELECT cities.name FROM cities WHERE cities.state_id = ( SELECT name FROM states WHERE name='California') ORDER BY cities.id ASC;
+SELECT cities.name FROM cities WHERE cities.state_id=( SELECT id FROM states WHERE name='California') ORDER BY cities.id ASC;
