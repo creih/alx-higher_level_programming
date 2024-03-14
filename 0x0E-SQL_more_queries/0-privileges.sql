c84c6f864e264daf7abd210ac4d328544b94175a
creih <iherr.9@gmail.com>
2024-03-14 11:12:55 -0700

    task 0 added the specific users mentioned at diff lines

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
 -- displaying all privileges of users
 -- nakoresheje show rats
-SHOW GRANTS FOR user_0d_1, user_0d_2;
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
