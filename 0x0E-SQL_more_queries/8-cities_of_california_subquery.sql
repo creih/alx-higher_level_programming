9abf2f89097045c9b86e0945b35133ecfd515cbc
creih <iherr.9@gmail.com>
2024-03-15 00:09:12 -0700

    added id in dis[lay columns

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,3 @@
 -- task 8 about selecting multiple fields from diff tables
 -- using select cyane
-SELECT name FROM cities WHERE state_id=(SELECT id FROM states WHERE name='California') ORDER BY cities.id ASC;
+SELECT id, name FROM cities WHERE state_id=(SELECT id FROM states WHERE name='California') ORDER BY cities.id ASC;
