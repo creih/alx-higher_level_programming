960ab91141951dd152079ebc1c01c46984692e4f
creih <iherr.9@gmail.com>
2024-03-19 15:41:28 +0200

    task 15 removed having statement

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,3 +1,3 @@
 -- task 15 about use of cout
 -- ikoreshwa rya function count()
-SELECT score, COUNT(score) FROM second_table GROUP BY score HAVING COUNT(score)>1;
+SELECT score, COUNT(score) as number FROM second_table GROUP BY score;
