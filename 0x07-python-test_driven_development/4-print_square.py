82f179c63e07ed17b77869c1599c0f718e9e34cd
creih <iherr.9@gmail.com>
2024-03-19 22:26:15 +0200

    used try and except

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -6,14 +6,20 @@ def print_square(size):
     """
     this is the print square to print square of #
     """
-    if size < 0:
-        raise ValueError("size must be >= 0")
-    if isinstance(size, float) and size < 0:
-        raise TypeError("size must be an integer")
-    if isinstance(size, int):
-        for ivert in range(0, size):
-            for yhori in range(0, size):
-                print("#", end="")
-            print()
-    if not isinstance(size, int):
-        raise TypeError("size must be an integer")
+    # if not type(size) is int:
+    #     raise TypeError("size must be an integer")
+    # if size < 0:
+    #     raise ValueError("size must be >= 0")
+    # if isinstance(size, float) and size < 0:
+    #     raise TypeError("size must be an integer")
+    try:
+        if isinstance(size, int):
+            for ivert in range(0, size):
+                for yhori in range(0, size):
+                    print("#", end="")
+                print()
+    except TypeError:
+        print("size must be an integer")
+    except ValueError:
+        print("size must be >= 0")
+# print_square(-0.8)
