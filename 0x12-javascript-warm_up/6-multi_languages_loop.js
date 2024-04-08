302ec940ccdfb0ac930cd2c368df8d629c80882f
creih <iherr.9@gmail.com>
2024-04-08 14:07:27 -0700

    task 6

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 let i = 0;
 const strArray = [
-'C is fun',
-'Python is cool',
-'JavaScript is amazing'
+  'C is fun',
+  'Python is cool',
+  'JavaScript is amazing'
 ];
 while (strArray.length > i) {
   console.log(strArray[i]);
