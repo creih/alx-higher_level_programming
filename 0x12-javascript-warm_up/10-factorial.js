67fc4921ac8df75486ba0d5a534102eeaaf6996d
creih <iherr.9@gmail.com>
2024-04-08 14:22:06 -0700

    task 10 semi compliant

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 const iya1 = parseInt(process.argv[2]);
-function factorial(n) {
-  if(isNaN(n) || n === 0) {
+function factorial (n) {
+  if (isNaN(n) || n === 0) {
     return (1);
   }
-	return (n * factorial(n - 1));
+  return (n * factorial(n - 1));
 }
 const res = factorial(iya1);
 console.log(res);
