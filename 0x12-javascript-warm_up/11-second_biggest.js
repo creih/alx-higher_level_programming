f203d6675c627d7cd2bf107e809908a004e932b9
creih <iherr.9@gmail.com>
2024-04-08 22:40:19 +0200

    task 1 changed few things

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
-const bare = parseInt(process.argv.slice(2).map(Number));
+const numbers = process.argv.slice(2).map(Number);
 
-bare.sort((a, b) => b- a);
-if (bare.length === 0 || bare.length === 1) {
+numbers.sort((a, b) => b - a);
+
+if (numbers.length === 0 || numbers.length === 1) {
   console.log(0);
-}  else {
-  console.log(bare[1]);
+  } else {
+  console.log(numbers[1]);
 }
