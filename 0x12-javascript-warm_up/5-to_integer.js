830da61122f253a84a06f46bbd46bc65b6b2fe76
creih <iherr.9@gmail.com>
2024-04-08 23:04:23 +0200

    task 5 removed tabs

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const iya1 = parseInt(process.argv[2]);
 if (!isNaN(iya1)) {
-	console.log(`My number: ${iya1}`);
+  console.log(`My number: ${iya1}`);
 } else {
-	console.log('Not a number');
+  console.log('Not a number');
 }
