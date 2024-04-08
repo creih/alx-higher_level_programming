181f43dd847d4f2f23f6c5408f713434fc9e80db
creih <iherr.9@gmail.com>
2024-04-08 22:20:30 +0200

    ask 9 done

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+/* function to add 2 args retrieved from terminal */
+const iya1 = parseInt(process.argv[2]);
+const iya2 = parseInt(process.argv[3]);
+
+function add(a, b) {
+  return (a + b);
+}
+console.log(add(iya1, iya2));
