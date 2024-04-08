12e51a0f9109d72cd030a6507fb8ab7006d277b4
creih <iherr.9@gmail.com>
2024-04-08 23:02:08 +0200

    task 5 semistandard compliant

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,10 +1,7 @@
 #!/usr/bin/node
 const iya1 = parseInt(process.argv[2]);
-if (!isNaN(iya1))
-{
+if (!isNaN(iya1)) {
 	console.log(`My number: ${iya1}`);
-}
-else
-{
+} else {
 	console.log('Not a number');
 }
