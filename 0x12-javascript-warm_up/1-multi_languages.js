079aca1b8fbbd79bf9f5778174315234ff190abb
creih <iherr.9@gmail.com>
2024-04-08 16:50:12 +0200

    trying to collect a non so necessary checker

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-/* task 1  to print three lines of string*/
+/* task 1  to print three lines of string */
 console.log('C is fun');
 console.log('Python is cool');
 console.log('JavaScript is amazing');
