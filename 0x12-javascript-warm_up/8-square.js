86d26d8d5cfea6856c16574a35dfc5372b6e5624
creih <iherr.9@gmail.com>
2024-04-08 22:11:49 +0200

    task 8ready to be checked

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+/* about printing a square of character usiin loops */
+const iya1 = process.argv[2];
+const ingano = parseInt(iya1);
+if (!isNaN(ingano)) {
+  for (let i = 0; i < ingano; i++) {
+    let row = '';
+	  for (let j = 0; j < ingano; j++) {
+	    row += 'X';
+	  }
+	  console.log(row);
+  }
+} else {
+  console.log('Missing size');
+}
