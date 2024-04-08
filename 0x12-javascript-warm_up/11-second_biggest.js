7e54af819a998b34156c6a6b3f49a8e6ba0585fd
creih <iherr.9@gmail.com>
2024-04-08 22:31:47 +0200

    task 11

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const bare = parseInt(process.argv.slice(2).map(bare));
+
+bare.sort((a, b) => b- a);
+if (bare.length === 0 || bare.length === 1) {
+  console.log(0);
+}  else {
+  console.log(bare[1]);
+}
