8c5776073162e88067f47ebd3a66b88c8254ba49
creih <iherr.9@gmail.com>
2024-04-09 15:23:59 -0700

    task 10

---
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+exports.converter = function (base) {
+  function convertToBase (number) {
+    if (number < base) {
+      return number.toString();
+    }
+    return convertToBase(Math.floor(number / base)) + (number % base).toString();
+  }
+  return convertToBase;
+};
