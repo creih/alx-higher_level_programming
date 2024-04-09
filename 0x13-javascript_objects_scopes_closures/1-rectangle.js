9201ddcc8cfb8c4da44586871ebd90618d248e07
creih <iherr.9@gmail.com>
2024-04-09 12:41:09 +0200

    task 1 and 0 and readme files

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// making constructor for the rectangle class
+class Rectangle {
+  let width, height;
+  constructor(w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}
---
+++ 0x13-javascript_objects_scopes_closures/README.md
@@ -0,0 +1,4 @@
+THis repo room is about furthering knowledge in javascript scripting
+
+Task 0: creation of empty class rectangle
+
