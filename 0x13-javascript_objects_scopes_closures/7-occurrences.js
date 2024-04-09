b8049f9b7363adc9202e8a43c86ed35b51c915cf
creih <iherr.9@gmail.com>
2024-04-09 15:07:25 -0700

    task 7

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,10 +1,9 @@
 exports.nbOccurences = function(list, searchElement) {
-
-let count = 0;
+  let count = 0;
   for (let i = 0; i < list.length; i++) {
     if (list[i] === searchElement) {
       count++;
     }
   }
-return count;
+  return count;
 };
