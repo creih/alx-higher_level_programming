eb1baf2f6dd1512c060a5b91b91c3a315fd8e491
creih <iherr.9@gmail.com>
2024-04-09 21:56:07 +0200

    task 2 added checks for if w, h are integers

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,7 +2,7 @@
 class Rectangle {
   constructor (w, h) {
     if (!Number.isIntger(w) || !Number.isInteger(h) || w <= 0 || h <= 0) {
-      return {};
+      return;
     }
     this.width = w;
     this.height = h;
