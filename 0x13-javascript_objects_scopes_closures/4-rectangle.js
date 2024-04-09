50d359c937da5405d2b804295107d75138d5e885
creih <iherr.9@gmail.com>
2024-04-09 22:56:35 +0200

    task 4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,30 @@
+#!/usr/bin/node
+/* task 3 */
+class Rectangle {
+  constructor (w, h) {
+    if (!Number.isInteger(w) || !Number.isInteger(h) || w <= 0 || h <= 0) {
+      return;
+    }
+    this.width = w;
+    this.height = h;
+  }
+
+  print () {
+    if (!this.width || !this.height) {
+      console.log('');
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+  rotate () {
+    let tp = this.width;
+    this.width = this.height;
+    this.heoght = tp;
+  }
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+module.exports = Rectangle;
