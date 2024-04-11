00d02e4315eddde0fb21430f4c0d16228c1f977a
creih <iherr.9@gmail.com>
2024-04-11 11:18:19 +0200

    task 15 was missing the descending

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,3 +1,3 @@
 -- task 15 about use of cout
 -- ikoreshwa rya function count()
-SELECT score, COUNT(score) as number FROM second_table GROUP BY score;
+SELECT score, COUNT(score) as number FROM second_table ORDER BY score DESC;
