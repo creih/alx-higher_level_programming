a43a793750dbcd5bb18dab3ec4e6eb8f45a46e9e
creih <iherr.9@gmail.com>
2024-04-13 21:57:21 +0200

    task 2 done

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+this is task 0 mentioned in the README file
+"""
+import sys
+import MySQLdb as msq
+if len(sys.argv) != 5:
+    print("Usage: python script.py <username> <password> <database>")
+    sys.exit(1)
+usrname = sys.argv[1]
+pswd = sys.argv[2]
+dtbs = sys.argv[3]
+data = sys.argv[4]
+
+dbcon = msq.connect(
+        host="localhost",
+        port=3306,
+        user=usrname,
+        passwd=pswd,
+        db=dtbs
+        )
+cursor = dbcon.cursor()
+qr = "SELECT * FROM states WHERE name=data"
+cursor.execute(qr)
+states = cursor.fetchall()
+for state in states:
+    print(state)
+cursor.close()
+dbcon.close()
