0007d66095e1834a2b96f536c77b1e62219f7e1c
creih <iherr.9@gmail.com>
2024-04-13 21:12:29 +0200

    task 1 changed the querry from 1 as tthey are almost the same

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+this is task 0 mentioned in the README file
+"""
+import sys
+import MySQLdb as msq
+if len(sys.argv) != 4:
+    print("Usage: python script.py <username> <password> <database>")
+    sys.exit(1)
+usrname = sys.argv[1]
+pswd = sys.argv[2]
+dtbs = sys.argv[3]
+
+dbcon = msq.connect(
+        host="localhost",
+        port=3306,
+        user=usrname,
+        passwd=pswd,
+        db=dtbs
+        )
+cursor = dbcon.cursor()
+qr = "SELECT * FROM states WHERE name LIKE 'A%' ORDER BY id ASC"
+cursor.execute(qr)
+states = cursor.fetchall()
+for state in states:
+    print(state)
