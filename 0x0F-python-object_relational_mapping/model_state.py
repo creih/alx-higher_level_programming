eb895a9bbf160798db1eba2abfdb526c082d7fe5
creih <iherr.9@gmail.com>
2024-04-14 21:55:36 +0200

    task 6

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+task model_state
+"""
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+
+# Define Base instance
+Base = declarative_base()
+
+class State(Base):
+    """state class"""
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
+
+
+engine = create_engine('mysql+mysqldb://root:root@localhost:3306/hbtn_0e_6_usa')
+
+Base.metadata.create_all(engine)
