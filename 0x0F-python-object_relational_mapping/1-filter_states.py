66dfa1c01c5e50c760c178e28708eb60836a3b41
creih <iherr.9@gmail.com>
2024-04-14 20:23:29 +0200

    task 1 added if name is main

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -4,28 +4,26 @@ this is task 0 mentioned in the README file
 """
 import sys
 import MySQLdb as msq
-if len(sys.argv) != 4:
-    print("Usage: python script.py <username> <password> <database>")
-    sys.exit(1)
-usrname = sys.argv[1]
-pswd = sys.argv[2]
-dtbs = sys.argv[3]
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: python script.py <username> <password> <database>")
+        sys.exit(1)
+    usrname = sys.argv[1]
+    pswd = sys.argv[2]
+    dtbs = sys.argv[3]
 
-dbcon = msq.connect(
+    dbcon = msq.connect(
         host="localhost",
         port=3306,
         user=usrname,
         passwd=pswd,
         db=dtbs
         )
-cursor = dbcon.cursor()
-qr = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC"
-cursor.execute(qr)
-states = cursor.fetchall()
-for state in states:
-    print(state)
-cursor.close()
-dbcon.close()
-
-if __name__ == "__main__":
-    pass
+    cursor = dbcon.cursor()
+    qr = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC"
+    cursor.execute(qr)
+    states = cursor.fetchall()
+    for state in states:
+        print(state)
+    cursor.close()
+    dbcon.close()
