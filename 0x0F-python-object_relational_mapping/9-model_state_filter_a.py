cb33957cb1cc8cb46febef4582f03b0fbfa8f12f
creih <iherr.9@gmail.com>
2024-04-14 22:21:34 +0200

    task 9

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+task 9 answer or response
+"""
+import sys
+import SQLAlchemy
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+def list_states_with_letter_a(username, password, database):
+    """ Create the engine"""
+    engine = create_engine(f'mysql+mysqldb://{username}:{password}@localhost:3306/{database}')
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    states = session.query(State).filter(State.name.like('%a%')).order_by(State.id).all()
+    for state in states:
+        print(f"{state.id}: {state.name}")
+    session.close()
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: python script.py <username> <password> <database>")
+        sys.exit(1)
+
+    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
+    list_states_with_letter_a(username, password, database)
