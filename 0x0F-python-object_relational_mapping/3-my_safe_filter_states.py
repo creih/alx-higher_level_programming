f3344194c5bd7f1f66e56aea2753c15fe5f02f99
creih <iherr.9@gmail.com>
2024-04-14 13:59:45 -0700

    task 3 pycoded

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -5,9 +5,16 @@ task 3 answer
 import MySQLdb
 import sys
 
+
 def filter_states_by_name(username, password, database, state_name):
     """Connect to the MySQL server"""
-    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
+    db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=username,
+            passwd=password,
+            db=database
+            )
     cursor = db.cursor()
     query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
     cursor.execute(query, (state_name,))
@@ -20,8 +27,11 @@ def filter_states_by_name(username, password, database, state_name):
 
 if __name__ == "__main__":
     if len(sys.argv) != 5:
-        print("Usage: python script.py <username> <password> <database> <state_name>")
+        print("Usage: python script.py <user> <pss> <dtbase> <state_name>")
         sys.exit(1)
 
-    username, password, database, state_name = sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
     filter_states_by_name(username, password, database, state_name)
