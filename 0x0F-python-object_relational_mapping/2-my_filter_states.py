1e976fae4c7714e8437b61109e9d33c8c27bd932
creih <iherr.9@gmail.com>
2024-04-15 13:41:29 -0700

    fixed the pycodestyling

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -5,11 +5,18 @@ this file is for task nbr 2
 import sys
 import MySQLdb
 
+
 def filter_states_by_name(username, password, database, state_name):
     """
     Connect to the MySQL server
     """
-    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
+    db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=username,
+            passwd=password,
+            db=database
+            )
     cursor = db.cursor()
     query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
     cursor.execute(query, (state_name,))
@@ -19,10 +26,14 @@ def filter_states_by_name(username, password, database, state_name):
     cursor.close()
     db.close()
 
+
 if __name__ == "__main__":
     if len(sys.argv) != 5:
-        print("Usage: python script.py <username> <password> <database> <state_name>")
+        print("Usage: python script.py with 4 args")
         sys.exit(1)
 
-    username, password, database, state_name = sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
     filter_states_by_name(username, password, database, state_name)
