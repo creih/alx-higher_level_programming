a838f2d6961d6eeda3f84604f0843df0ab594b3e
creih <iherr.9@gmail.com>
2024-04-15 14:18:40 -0700

    task 6 documented and pycoded

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -4,9 +4,7 @@ task model_state is about joining sql alchemy in our python codes
 """
 import SQLAlchemy
 from sqlalchemy import Column, Integer, String, create_engine
-from sqlalchemy.ext.declarative import declarative_base
-
-Base = declarative_base()
+from sqlalchemy.ext.declarative import declarative_base as Base
 
 
 class State(Base):
