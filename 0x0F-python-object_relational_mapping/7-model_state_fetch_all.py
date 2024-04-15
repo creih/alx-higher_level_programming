fd97473ef58af41373995b773da3e9c374619546
creih <iherr.9@gmail.com>
2024-04-15 14:29:47 -0700

    task 7 dcumented

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -7,8 +7,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
+
 def list_states(username, password, database):
-    """Create the engine"""
+    """
+    Creates the engine then displays the states but using alchemy
+    not mysql or filestorage
+    """
     engine = create_engine(f'mysql+mysqldb://{username}:{password}@localhost:3306/{database}')
     Session = sessionmaker(bind=engine)
     session = Session()
@@ -16,6 +20,7 @@ def list_states(username, password, database):
     for state in states:
         print(f"{state.id}: {state.name}")
     session.close()
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 4:
