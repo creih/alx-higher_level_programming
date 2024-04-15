8d1465f6b591194363351c15349900e89eae1516
creih <iherr.9@gmail.com>
2024-04-15 22:24:54 +0200

    task 4 used join

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -10,7 +10,8 @@ def list_cities(username, password, database):
     """list of cities from task 4"""
     db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
     cursor = db.cursor()
-    query = "SELECT id, name FROM cities ORDER BY id ASC"
+    query = "SELECT cities.id, cities.name,states.name FROM cities \
+            JOIN states ON cities.state_id = states.id  ORDER BY cities.id ASC"
     cursor.execute(query)
     cities = cursor.fetchall()
     for city in cities:
