dba69315b8a280e6b23f51d459c3d893a50f1661
creih <iherr.9@gmail.com>
2024-04-19 13:34:19 -0700

    task 6 removed the connection

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,26 +1,38 @@
 #!/usr/bin/python3
-"""
-task model_state is about joining sql alchemy in our python codes
-"""
-import SQLAlchemy
-from sqlalchemy import Column, Integer, String, create_engine
+"""This module contains the definition of the State class."""
+
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
+
 Base = declarative_base()
 
-
 class State(Base):
-    """
-    state class meant to define how the id should be
-    """
+    """Class representing a State."""
+
     __tablename__ = 'states'
 
-    id = Column(
-            Integer,
-            primary_key=True,
-            nullable=False,
-            autoincrement=True
-            )
-    name = Column(
-            String(128),
-            nullable=False
-            )
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
+
+
+if __name__ == "__main__":
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+
+    create_engine('mysql://username:password@localhost:3306/database')
+
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    new_state = State(name='New State')
+    session.add(new_state)
+    session.commit()
+
+
+    states = session.query(State).all()
+    for state in states:
+        print(state.id, state.name)
+
+    session.close()
