1e0bb53073ee89f297da59849511af149a05bbc8
creih <iherr.9@gmail.com>
2024-04-19 14:42:29 -0700

    yask 0

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -8,9 +8,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
+
 def print_state_by_name(username, password, database, state_name):
-    """Connect to the MySQL server and print the State object with the given name."""
-    engine = create_engine(f'mysql://{username}:{password}@localhost:3306/{database}')
+    """Connect to the MySQL server and print the State object with name."""
+    engine = create_engine(f'mysql://{username}:{password}\
+            @localhost:3306/{database}')
     Session = sessionmaker(bind=engine)
     session = Session()
     state = session.query(State).filter(State.name == state_name).first()
@@ -20,9 +22,11 @@ def print_state_by_name(username, password, database, state_name):
         print("Not found")
     session.close()
 
+
 if __name__ == "__main__":
     if len(sys.argv) != 5:
-        print("Usage: python script.py <username> <password> <database> <state_name>")
+        print("Usage: python script.py <username> \
+                <password> <database> <state_name>")
         sys.exit(1)
     username = sys.argv[1]
     password = sys.argv[2]
