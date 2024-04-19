30dcf273a6bdb617a350309bbf9862b93b11c731
creih <iherr.9@gmail.com>
2024-04-19 14:12:03 -0700

    task 8

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -11,15 +11,18 @@ from model_state import Base, State
 
 def print_first_state(username, password, database):
     """Connect to the MySQL server and print the first State object."""
+
     engine = create_engine(f'mysql://{username}:\
             {password}@localhost:3306/{database}')
     Session = sessionmaker(bind=engine)
     session = Session()
+
     first_state = session.query(State).order_by(State.id).first()
     if first_state:
         print(first_state.id, first_state.name)
     else:
         print("No records found in the database.")
+
     session.close()
 
 
@@ -27,8 +30,11 @@ if __name__ == "__main__":
     if len(sys.argv) != 4:
         print("Usage: python script.py <username> <password> <database>")
         sys.exit(1)
+
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
+
     from model_state import Base, State
+
     print_first_state(username, password, database)
