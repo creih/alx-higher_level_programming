7dd99adb342335d6a6cf19959a1d76a5486f9087
creih <iherr.9@gmail.com>
2024-04-19 15:36:32 -0700

    task 102 added import sqlalchemy

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Script to list all City objects from the db."""
-
+import sqlalchemy
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
