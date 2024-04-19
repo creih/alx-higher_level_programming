a8cc75bfd55101b9a03f1d4e0c8fee27b460d162
creih <iherr.9@gmail.com>
2024-04-19 13:14:32 -0700

    task 5 used join when printing

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,44 +1,38 @@
 #!/usr/bin/python3
 """
-task 5 about displaying city be state
+Task 5: Display cities by state
 """
+
 import sys
 import MySQLdb
 
-
-def list_cities_by_state(
-        username,
-        password,
-        database,
-        state_name
-        ):
+def list_cities_by_state(username, password, database, state_name):
     """Connect to the MySQL server and display cities"""
     db = MySQLdb.connect(
-            host="localhost",
-            port=3306,
-            user=username,
-            passwd=password,
-            db=database
-            )
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
     cursor = db.cursor()
     query = "SELECT cities.name FROM cities \
              JOIN states ON cities.state_id = states.id \
              WHERE states.name = %s ORDER BY cities.id ASC"
     cursor.execute(query, (state_name,))
     rows = cursor.fetchall()
-    for row in rows:
-        print(', '.join(row[0] for row in rows))
+    cities = ', '.join(row[0] for row in rows)
+    print(cities)
     cursor.close()
     db.close()
-
 
 if __name__ == "__main__":
     if len(sys.argv) != 5:
         print("Usage: python script.py with 4 args")
         sys.exit(1)
 
-    username = sys.argv[1],
-    password = sys.argv[2],
-    database = sys.argv[3],
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
     state_name = sys.argv[4]
     list_cities_by_state(username, password, database, state_name)
