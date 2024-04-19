771d688f6a76782f54a02b22a74aa677feb639bf
creih <iherr.9@gmail.com>
2024-04-19 13:50:30 -0700

    task 7 remo

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """
-task 7 response is all in this file
+Script to list all State objects from the database hbtn_0e_6_usa.
 """
+
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -9,16 +10,18 @@ from model_state import Base, State
 
 
 def list_states(username, password, database):
-    """
-    Creates the engine then displays the states but using alchemy
-    not mysql or filestorage
-    """
-    engine = create_engine(f'mysql+mysqldb://{username}:{password}@localhost:3306/{database}')
+    """Connect to the MySQL server and list all State objects."""
+
+    engine = create_engine(f'mysql://{username}:{password}\
+            @localhost:3306/{database}')
+
     Session = sessionmaker(bind=engine)
     session = Session()
+
     states = session.query(State).order_by(State.id).all()
     for state in states:
-        print({state.id}, {state.name})
+        print(state.id, state.name)
+
     session.close()
 
 
@@ -27,5 +30,12 @@ if __name__ == "__main__":
         print("Usage: python script.py <username> <password> <database>")
         sys.exit(1)
 
-    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    from model_state import Base, State
+
+    Base.metadata.create_all(create_engine(f'mysql://{username}:\
+            {password}@localhost:3306/{database}'))
     list_states(username, password, database)
