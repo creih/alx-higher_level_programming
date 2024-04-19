653d98c1c2447c01dacf7124efbb18722a59dfff
creih <iherr.9@gmail.com>
2024-04-19 14:33:37 -0700

    task 9

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,31 +1,30 @@
 #!/usr/bin/python3
-"""
-task 9 answer or response
-"""
 import sys
-import SQLAlchemy
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
+def list_states_with_a(username, password, database_name):
+    """
+    this function is for listing all states that contain letter a
+    """
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(username, password, database_name),
+                           pool_pre_ping=True)
 
-def list_states_with_letter_a(username, password, database):
-    """ Create the engine for sqlalchemy thingies"""
-    engine = create_engine(f'mysql+mysqldb://{username}:\
-            {password}@localhost:3306/{database}')
     Session = sessionmaker(bind=engine)
     session = Session()
-    states = session.query(State).filter(State.name.like('\
-            %a%')).order_by(State.id)
-    for state in states:
-        print(state.id, state.name)
-    session.close()
+
+    states_with_a = session.query(State).filter(State.name.like('%a%'))
+        .order_by(State.id)
+
+    for state in states_with_a:
+        print("{}: {}".format(state.id, state.name))
 
 
 if __name__ == "__main__":
     if len(sys.argv) != 4:
-        print("Usage: python script.py <username> <password> <database>")
+        print("Usage: python script.py <username> <password> <database_name>")
         sys.exit(1)
-
-    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
-    list_states_with_letter_a(username, password, database)
+    username, password, database_name = sys.argv[1], sys.argv[2], sys.argv[3]
+    list_states_with_a(username, password, database_name)
