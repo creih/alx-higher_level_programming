79b8fa76883143a481f4181306a0be9c634e2806
creih <iherr.9@gmail.com>
2024-04-19 14:00:26 -0700

    task 8

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,26 +1,41 @@
 #!/usr/bin/python3
-""".alx-token.txt.swp"""
+"""
+Script to print the first State object from the database hbtn_0e_6_usa.
+"""
+
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
+
 def print_first_state(username, password, database):
-    """ Create the engine """
-    engine = create_engine(f'mysql+mysqldb://{username}:{password}@localhost:3306/{database}')
+    """Connect to the MySQL server and print the first State object."""
+
+    engine = create_engine(f'mysql://{username}:\
+            {password}@localhost:3306/{database}')
+
     Session = sessionmaker(bind=engine)
     session = Session()
+
     first_state = session.query(State).order_by(State.id).first()
     if first_state:
-        print(f"{first_state.id}: {first_state.name}")
+        print(first_state.id, first_state.name)
     else:
         print("Nothing")
+
     session.close()
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 4:
         print("Usage: python script.py <username> <password> <database>")
         sys.exit(1)
 
-    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    from model_state import Base, State
+
     print_first_state(username, password, database)
