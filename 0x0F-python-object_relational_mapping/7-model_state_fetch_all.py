7a48bfd67f7e26271c007a818c61272e36077a3b
creih <iherr.9@gmail.com>
2024-04-19 13:41:55 -0700

    task 7 removed the connection

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -18,7 +18,7 @@ def list_states(username, password, database):
     session = Session()
     states = session.query(State).order_by(State.id).all()
     for state in states:
-        print(f"{state.id}: {state.name}")
+        print({state.id}, {state.name})
     session.close()
 
 
