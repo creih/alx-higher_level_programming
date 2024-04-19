68f9d35d9ed46b711f19b377bb6c6aabbc8dcc36
creih <iherr.9@gmail.com>
2024-04-19 15:06:56 -0700

    task 13

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-Script that deletes all State objects with a name containing the letter a
-from e database hbtn_0e_6_usa
+Script to delete all State objects with a name containing the letter 'a'
+from the database hbtn_0e_6_usa.
 """
 
 import sys
@@ -9,21 +9,35 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
+
+def delete_states_with_a(username, password, database):
+    """
+    Connect to the MySQL server and delete all
+    State objects with a name containing 'a'.
+    """
+    engine = create_engine(f'mysql://{username}:{password}\
+            @localhost:3306/{database}')
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    states = session.query(State).filter(State.name.like('%a%')).all()
+    for state in states:
+        session.delete(state)
+    session.commit()
+
+    session.close()
+
+
 if __name__ == "__main__":
     if len(sys.argv) != 4:
-        print("Usage: {} username password database".format(sys.argv[0]))
+        print("Usage: python script.py <username> <password> <database>")
         sys.exit(1)
+
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(username, password, database),
-                           pool_pre_ping=True)
-    Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    states_to_delete = session.query(State).filter(State.name.like('%a%')).all()
-    for state in states_to_delete:
-        session.delete(state)
-    session.commit()
-    session.close()
+
+    from model_state import Base, State
+
+    delete_states_with_a(username, password, database)
