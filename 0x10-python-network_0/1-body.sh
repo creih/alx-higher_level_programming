dcacacf7aaddf12b3972f5d9cf8565a90c1f089b
creih <iherr.9@gmail.com>
2024-04-25 13:47:30 -0700

    task 1 shortened

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # this one displays body of get
-curl -s -o /dev/null -w "%{http_code}\n%{body}" "$1" | awk 'NR==1{if($1==200)print $2;else print "Error: Unexpected status code "$1}'
+curl -s "$1" | tail -n 1
