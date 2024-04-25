12b64afbbad2868ef63891fb16e430e43c1d4bb1
creih <iherr.9@gmail.com>
2024-04-25 15:03:45 -0700

    task 100 about displaying status codes

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # advanced task about displaying status code
-status_code=$(curl -s -o /dev/null -w "%{http_code}" "$1")
-echo "$status_code"
+status_code=$(curl -s -o /dev/null -w "%{http_code}" "$1") echo "$status_code"
