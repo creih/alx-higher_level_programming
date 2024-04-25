420639ee755901b144d7c24c6af948f8d97aedcc
creih <iherr.9@gmail.com>
2024-04-25 14:38:38 -0700

    task 6 txt and py file

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" this file contains the python file for etting the peak in list of ints"""
+def find_peak(list_of_integers):
+    """ this function is for peak detection"""
+    if not list_of_integers:
+        return None
+
+    left, right = 0, len(list_of_integers) - 1
+
+    while left < right:
+        mid = (left + right) // 2
+        if list_of_integers[mid] < list_of_integers[mid + 1]:
+            left = mid + 1
+        else:
+            right = mid
+
+    return list_of_integers[left]
