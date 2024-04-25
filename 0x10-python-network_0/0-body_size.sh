750a5767dadf8edd338d4fc25573ee87ab4b2516
creih <iherr.9@gmail.com>
2024-04-25 12:44:11 -0700

    task 0 added a 3 lines long

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,17 +1,3 @@
-#!/usr/bin/env bash
-# Check if a URL is provided as an argument
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-url=$1
-
-temp_file=$(mktemp)
-curl -s -o "$temp_file" -w "%{size_download}" "$url" > /dev/null
-
-size=$(cat "$temp_file")
-
-echo "Size of the response body: $size bytes"
-
-rm "$temp_file"
+#!/bin/bash
+# Script: to get size of curl response
+curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
