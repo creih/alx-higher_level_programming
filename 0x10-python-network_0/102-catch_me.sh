38e13fa88b250e426afcef4fb77a23cfc6d87053
creih <iherr.9@gmail.com>
2024-04-30 14:10:34 -0700

    task 102 on proper shebang

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/env bash
+#!/bin/bash
 # TASK TO DISPLAY CATCH ME IF YOU CAN STRertonSchool
 curl -sX PUT -d "user_id=98" -H "Origin: HolbertonSchool" 0.0.0.0:5000/catch_me --output /dev/null
