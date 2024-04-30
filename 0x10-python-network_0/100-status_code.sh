f25e49a1b0674e9313c314558697c5096b3ef619
creih <iherr.9@gmail.com>
2024-04-30 05:18:43 -0700

    task 100 used echo before

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # advanced task about displaying status code
-status_code=$(curl -s -o /dev/null -w "%{http_code}" "$1")| echo "$status_code"
+echo status_code=$(curl -s -o /dev/null -w "%{http_code}" "$1")
